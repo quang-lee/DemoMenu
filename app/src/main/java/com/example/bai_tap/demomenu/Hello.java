60cf47e757d29b4f8c379bb5bcd77b7f687a308d
quang-lee <levanquang.hd.97@gmail.com>
2019-07-20 21:15:16 +0700

    them moi

--- app/src/main/java/com/example/bai_tap/demomenu/Hello.java
+++ app/src/main/java/com/example/bai_tap/demomenu/Hello.java
@@ -1,4 +1,11 @@
 package com.example.bai_tap.demomenu;
 
-public class Hello {
+import android.app.Application;
+
+public class Hello extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
